a0924eddfaf370c97b0993fb00347a74238de923
Subhradip Mishra <mishrasubradip2005@gmail.com>
2026-09-26 00:02:20 +0530

    Fixed api issue

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://resiflowproject.onrender.com/',
+    baseURL: import.meta.env.VITE_API_URL || 'https://resiflowproject.onrender.com/api/v1',
     withCredentials: true, // Important for cookies
 });
 
